0893c251879efdaf0df872f44ea25bba3e9dc4ea
Marek Števuliak <stevuliak.m@gmail.com>
2017-03-26 16:17:38 +0200

    Changed additional libs

--- openCV_test/DBScan/Defs.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include "DataPoint.h"
-
-class Cluster; //to avoid circular depency DBScan <-> Cluster
-
-typedef std::vector<DataPoint *> vector_t;
-typedef std::vector<Cluster *> cluster_vec_t;
